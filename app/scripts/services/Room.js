5ff8353fcfe544378e7f6b6da7c6cd2a650c153e
Archita Patel <architapatelis@gmail.com>
2017-03-20 20:55:36 -0700

    checkpoint-3 create chat rooms

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,12 +1,21 @@
+// remember Room.js is a service. we don't use 'this' to refer to 'Room' like we would in a controller. 
 (function() {
     function Room($firebaseArray) {
-        
+        var Room = {};
         var ref = firebase.database().ref().child("rooms");
+        //get array of rooms stored in database on firebase
         var rooms = $firebaseArray(ref);
         
-        return {
-            all: rooms
+        // makes 'rooms' array public. used in home.html
+        Room.all = rooms;
+        
+        // to add newly created 'room' to the 'rooms' array.
+        //call in modalCtrl.js
+        Room.add = function (room) {
+            rooms.$add(room);
         };
+        
+        return Room;
     }
     
     angular
